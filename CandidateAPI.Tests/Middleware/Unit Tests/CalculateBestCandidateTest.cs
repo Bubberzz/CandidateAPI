da8aad94edcf06ea479eb5685424d2bf4d11a468
Stan Bubbers <stan.bubbers@asos.com>
2020-11-23 10:17:07 +0000

    Add CalculateBestCandidate + unit test + FluentAssertions package for unit testing
    CalculateBestCandidate contains logic that determines best candidate based on skill set

---
+++ CandidateAPI.Tests/Middleware/Unit Tests/CalculateBestCandidateTest.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using CandidateAPI.Middleware;
+using CandidateAPI.Models;
+using FluentAssertions;
+using Xunit;
+
+namespace CandidateAPI.Tests.Middleware.Unit_Tests
+{
+    public class CalculateBestCandidateTest
+    {
+
+        [Theory]
+        [InlineData("java", "sql", "csharp", "python")]
+        public void CalculateBestSkillMatchShouldReturnList(params string[] inputStrings)
+        {
+            // Arrange 
+            var getBestMatch = new CalculateBestCandidate();
+            var mockCandidates = new List<Candidate>
+            {
+                new Candidate()
+                {
+                    Id = "userid8723",
+                    Name = "John Wick",
+                    Skills = new[] {"java", "sql", "html"}
+                },
+                new Candidate()
+                {
+                    Id = "userid8724",
+                    Name = "Dr Phil",
+                    Skills = new[] {"java", "sql", "html", "csharp"}
+                },
+                new Candidate()
+                {
+                    Id = "userid8725",
+                    Name = "Will Smith",
+                    Skills = new[] {"java", "sql", "csharp", "python", "internet explorer"}
+                },
+            };
+
+            var expectedResult = new List<Candidate>
+            {
+                new Candidate()
+                {
+                    Id = "userid8725",
+                    Name = "Will Smith",
+                    Skills = new[] {"java", "sql", "csharp", "python", "internet explorer"},
+                    SkillMatch = 4
+                }
+            };
+
+            // Act 
+            var result = getBestMatch.ReturnResult(inputStrings, mockCandidates);
+
+            // Assert
+            expectedResult.Should().BeEquivalentTo(result);
+        }
+
+        [Theory]
+        [InlineData("csharp", "python")]
+        public void CalculateBestSkillMatchShouldReturnNothing(params string[] inputStrings)
+        {
+            // Arrange 
+            var getBestMatch = new CalculateBestCandidate();
+            var mockCandidates = new List<Candidate>
+            {
+                new Candidate()
+                {
+                    Id = "userid8723",
+                    Name = "John Wick",
+                    Skills = new[] {"java"}
+                },
+                new Candidate()
+                {
+                    Id = "userid8724",
+                    Name = "Dr Phil",
+                    Skills = new[] {"sql", "html", "photoshop"}
+                },
+                new Candidate()
+                {
+                    Id = "userid8725",
+                    Name = "Will Smith",
+                    Skills = new[] {"java", "sql", "internet explorer"}
+                },
+            };
+
+            var expectedResult = new List<Candidate>();
+
+            // Act 
+            var result = getBestMatch.ReturnResult(inputStrings, mockCandidates);
+
+            // Assert
+            expectedResult.Should().BeEquivalentTo(result);
+        }
+    }
+}
---
+++ CandidateAPI/Middleware/CalculateBestCandidate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandidateAPI.Models;
+
+namespace CandidateAPI.Middleware
+{
+    public class CalculateBestCandidate
+    {
+        public List<Candidate> ReturnResult(string[] skills, List<Candidate> candidates)
+        {
+            var result = new List<Candidate>();
+
+            foreach (var candidate in candidates)
+            {
+                var skillsMatch = (
+                    from skillInput
+                        in skills
+                    let skillsArray = candidate.Skills
+                    where skillsArray.Any(skill => skillInput == skill)
+                    select skillInput).Count();
+                if (skillsMatch == 0) continue;
+                candidate.SkillMatch = skillsMatch;
+                result.Add(candidate);
+            }
+
+            if (result.Count == 0)
+            {
+                return result;
+            }
+
+            var bestMatch = result.OrderByDescending(x => x.SkillMatch).First();
+            result.Clear();
+            result.Add(bestMatch);
+            return result;
+        }
+    }
+}
