0e998f7703ee327a47c1e11942c8b947e4e3795d
Stan Bubbers <stan.bubbers@asos.com>
2020-11-23 08:50:43 +0000

    Add Candidate model

---
+++ CandidateAPI/Models/Candidate.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CandidateAPI.Models
+{
+    public class Candidate
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "please add at least one skill"), MinLength(1)]
+        public string[] Skills { get; set; }
+
+        public int SkillMatch { get; set; }
+    }
+}
