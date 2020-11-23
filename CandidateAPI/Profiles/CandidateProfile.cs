102db7ba1846883e9a968a080f19957738f52e2f
Stan Bubbers <stan.bubbers@asos.com>
2020-11-23 10:32:29 +0000

    Add automapper profile

---
+++ CandidateAPI/Profiles/CandidateProfile.cs
@@ -0,0 +1,15 @@
+﻿using CandidateAPI.Dtos;
+using CandidateAPI.Models;
+using AutoMapper;
+
+namespace CandidateAPI.Profiles
+{
+    public class CandidateProfile : Profile
+    {
+        public CandidateProfile()
+        {
+            CreateMap<Candidate, CandidateReadDto>();
+            CreateMap<CandidateCreateDto, Candidate>();
+        }
+    }
+}
