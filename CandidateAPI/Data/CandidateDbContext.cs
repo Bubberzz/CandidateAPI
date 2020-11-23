3d08d13edfeff2a78651a37b3ecffb05b8303808
Stan Bubbers <stan.bubbers@asos.com>
2020-11-23 09:27:17 +0000

    Add database context

---
+++ CandidateAPI/Data/CandidateDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandidateAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CandidateAPI.Data
+{
+    public class CandidateDbContext : DbContext
+    {
+        public CandidateDbContext(DbContextOptions<CandidateDbContext> options) : base(options) { }
+        public DbSet<Candidate> Candidates { get; set; }
+    }
+}
