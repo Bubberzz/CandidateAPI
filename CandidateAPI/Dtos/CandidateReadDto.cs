226781b7dd53274f05874cc6c2bc07609d306617
Stan Bubbers <stan.bubbers@asos.com>
2020-11-23 10:24:11 +0000

    Add data transfer objects for reading and creating + automapper package

---
+++ CandidateAPI/Dtos/CandidateReadDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CandidateAPI.Dtos
+{
+    public class CandidateReadDto
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string[] Skills { get; set; }
+    }
+}
